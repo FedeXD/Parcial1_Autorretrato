70a0c9fd2f05537b26e29cf9238a7c510c6fe382
FedeXD <82965865+FedeXD@users.noreply.github.com>
2023-05-22 18:22:47 -0300

    Esquivar a los otros espermatozoides

--- Assets/Scripts/MovimientoEspermatozoies.cs
+++ Assets/Scripts/MovimientoEspermatozoies.cs
@@ -6,11 +6,12 @@ public class MovimientoEspermatozoies : MonoBehaviour
     public List<GameObject> waypoints;
     public float velocidadInicial = 2;
     public float incrementoVelocidad = 0.5f;
+    public float distanciaDeteccion = 2f; // Distancia para detectar otros espermatozoides
+    public float distanciaEvasion = 3f; // Distancia para evadir otros espermatozoides
+    public float evasionForce = 10f; // Fuerza de evasión
     int index = 0;
     public bool seRepite;
     private float velocidadActual;
-
-
     private void Start()
     {
         velocidadActual = velocidadInicial;
@@ -26,6 +27,21 @@ public class MovimientoEspermatozoies : MonoBehaviour
     {
         Vector3 destino = waypoints[index].transform.position;
         Vector3 siguientePosicion = Vector3.MoveTowards(transform.position, destino, velocidadActual * Time.deltaTime);
+
+        // Comprobar si hay otros espermatozoides cercanos
+        Collider[] colliders = Physics.OverlapSphere(transform.position, distanciaDeteccion);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("Espermatozoide") && collider.gameObject != gameObject)
+            {
+                // Calcular la dirección para evadir al otro espermatozoide
+                Vector3 evasionDirection = (transform.position - collider.transform.position).normalized;
+
+                // Aplicar fuerza para evadir
+                GetComponent<Rigidbody>().AddForce(evasionDirection * evasionForce, ForceMode.Acceleration);
+            }
+        }
+
         transform.position = siguientePosicion;
 
         float distancia = Vector3.Distance(transform.position, destino);
