747fe627612feb2e74bcbaae7da01f567f211076
FedeXD <82965865+FedeXD@users.noreply.github.com>
2023-05-24 16:49:05 -0300

    Hacer que el brazo baje al hacer click

--- Assets/Scripts/Figuras geometricas/BrazoMovible.cs
+++ Assets/Scripts/Figuras geometricas/BrazoMovible.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class BrazoMovible : MonoBehaviour
 {
-    private float velocidad = 5f; // Velocidad de movimiento del objeto
+    private float velocidad = 5f; //Velocidad de movimiento del objeto
+    private Vector3 posicionInicial;
+    private bool clicSostenido = false; //Indica si se está sosteniendo el clic
+    private Rigidbody rb;
 
-
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        posicionInicial = transform.position;
+    }
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X"); // Obtén el movimiento horizontal del mouse
-        float mouseY = Input.GetAxis("Mouse Y"); // Obtén el movimiento vertical del mouse
+        float mouseX = Input.GetAxis("Mouse X"); 
+        float mouseY = Input.GetAxis("Mouse Y"); 
 
-        Vector3 movement = new Vector3(mouseX, 0f, mouseY) * velocidad * Time.deltaTime; // Calcula el vector de movimiento
+        Vector3 movement = new Vector3(mouseX, 0f, mouseY) * velocidad * Time.deltaTime; //Calcula el vector de movimiento
 
-        transform.Translate(movement); // Mueve el objeto en la dirección calculada
+        if (Input.GetMouseButton(0)) //Si se mantiene el click izquierdo del mouse
+        {
+            clicSostenido = true;
+            movement += Vector3.down * velocidad * Time.deltaTime; //Le da movimiento hacia abajo si se sostiene el click
+        }
+        else
+        {
+            if (clicSostenido) //Si se soltó el clic, se mueve el objeto a la posición inicial
+            {
+                clicSostenido = false;
+                rb.MovePosition(posicionInicial); 
+            }
+        }
+
+        rb.MovePosition(rb.position + movement); //Mueve el objeto 
     }
+
 }
